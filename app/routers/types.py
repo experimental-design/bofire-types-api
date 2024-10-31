a249662eb0e4be18f2e04727e9b55953ad62c31b
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-10-31 23:05:15 +0100

    apply linting

--- app/routers/types.py
+++ app/routers/types.py
@@ -1,48 +1,61 @@
 from typing import Dict, List
 
 from fastapi import APIRouter, HTTPException
-from pydantic import ValidationError
-
 from models.type import AllTypes, GroupTypes, Type, ValidationResult
 from mytypes.all import ALL_CLASSES, ALL_TYPES
+from pydantic import ValidationError
+
 
 router = APIRouter(prefix="", tags=["types"])
+
 
 @router.get("/keys", response_model=Dict[str, List[str]])
 def get_all_keys() -> Dict[str, List[str]]:
     return {
-        group_key: [
-            type_key
-            for type_key in types
-        ]
+        group_key: [type_key for type_key in types]  # noqa: C416
         for group_key, types in ALL_TYPES.items()
     }
+
 
 @router.get("/types", response_model=AllTypes)
 def get_all_types() -> AllTypes:
     return ALL_TYPES
 
+
 @router.get("/types/{groupKey}", response_model=GroupTypes)
 def get_group_types(groupKey: str) -> GroupTypes:
     try:
         return ALL_TYPES[groupKey]
-    except KeyError as e:
+    except KeyError:
         raise HTTPException(status_code=404, detail=f"No group {groupKey} exists.")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"A server error occurred. Details: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"A server error occurred. Details: {e}"
+        )
+
 
 @router.get("/types/{groupKey}/{typeKey}", response_model=Type)
 def get_type(groupKey: str, typeKey: str) -> Type:
     try:
         return ALL_TYPES[groupKey][typeKey]
-    except KeyError as e:
-        raise HTTPException(status_code=404, detail=f"No type {typeKey} for group {groupKey} exists.")
+    except KeyError:
+        raise HTTPException(
+            status_code=404, detail=f"No type {typeKey} for group {groupKey} exists."
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"A server error occurred. Details: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"A server error occurred. Details: {e}"
+        )
 
 
-@router.post("/types/{groupKey}/{typeKey}/validate-type-schema", response_model=ValidationResult)
-def validate_type_schema(groupKey: str, typeKey: str, schema: dict,) -> ValidationResult:
+@router.post(
+    "/types/{groupKey}/{typeKey}/validate-type-schema", response_model=ValidationResult
+)
+def validate_type_schema(
+    groupKey: str,
+    typeKey: str,
+    schema: dict,
+) -> ValidationResult:
     try:
         cls_ = ALL_CLASSES[groupKey][typeKey]
         try:
@@ -54,8 +67,10 @@ def validate_type_schema(groupKey: str, typeKey: str, schema: dict,) -> Validati
         except Exception as e:
             return ValidationResult(valid=False, details=str(e))
     except KeyError:
-        raise HTTPException(status_code=404, detail=f"No type {typeKey} for group {groupKey} exists.")
+        raise HTTPException(
+            status_code=404, detail=f"No type {typeKey} for group {groupKey} exists."
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"A server error occurred. Details: {e}")
-
-
+        raise HTTPException(
+            status_code=500, detail=f"A server error occurred. Details: {e}"
+        )

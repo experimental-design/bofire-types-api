e8e1d0f91016dd340c809924dd0e5c8e3d82aa21
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-10-31 22:53:24 +0100

    add lint pipeline

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from starlette.responses import RedirectResponse
 
 from routers.types import router as types_router
 
-app = FastAPI(title="Bofire Types API", version="0.1.0", root_path="/")
+app = FastAPI(title="BoFire Types API", version="0.1.0", root_path="/")
 
 
 @app.get("/", include_in_schema=False)
